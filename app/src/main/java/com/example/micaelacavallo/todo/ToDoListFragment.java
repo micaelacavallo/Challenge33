e504afaa357ec8b79c040f32092b7a609a5490d6
micaela.cavallo <micaela.cavallo@globant.com>
2015-02-05 11:44:39 -0300

    As a user, I want to see an “Add Task” button in the toolbar

--- app/src/main/java/com/example/micaelacavallo/todo/ToDoListFragment.java
+++ app/src/main/java/com/example/micaelacavallo/todo/ToDoListFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,5 +23,38 @@ public class ToDoListFragment extends ListFragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_to_do_list, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_fragment_add_task, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Boolean handled = false;
+
+        switch (id)
+        {
+            case R.id.action_add:
+                handled=true;
+                break;
+        }
+        if (!handled) {
+            handled = super.onOptionsItemSelected(item);
+        }
+
+        return handled;
     }
 }
